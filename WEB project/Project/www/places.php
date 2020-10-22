d9c3fc8582db4ac7e7923b4002dcbf82e79b506d
mujotwentyfour <72901828+mujotwentyfour@users.noreply.github.com>
2020-10-22 08:31:37 +0600

    Add files via upload

---
+++ WEB project/Project/www/places.php
@@ -0,0 +1,179 @@
+<html>
+<head>
+	<title>PLACES</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="header">
+		<span class="logo">PLACES</span>
+	</div>
+	
+	<nav class="navbar navbar-expand-lg navbar-light ">
+		<div class="container">
+			<div class="collapse navbar-collapse justify-content-center">
+				<ul class="nav navbar-nav navbar-center">
+				
+					<li class="nav-item ">
+						<a href="index.php" class="nav-link">HOME</a>
+					</li>
+					<li class="nav-item active">
+						<a href="places.php" class="nav-link">PLACES</a>
+					</li>
+					<li class="nav-item">
+						<a href="gallery.php" class="nav-link">GALLERY</a>
+					</li>
+					<li class="nav-item">
+						<a href="#" class="nav-link">LOGIN</a>
+					</li>
+				
+					
+				</ul>
+			</div>
+		</div>
+	</nav>
+	
+	<div class="container">
+	
+	
+		<div class="row">
+			<div class="panel col-sm-3">
+				<div class="float-center">
+						<img class="bigPicture bp1" src="img/bayterek.jpg" width="100%"></img>
+				</div>
+				<div class="float-center">
+					<div class="panelText">
+						<h3 style="margin-top:15px;">BAITEREK<h3>
+						<p class="panelP">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+						<button type="button" class="btn btn-dark btn-lg">VIEW ALL TICKETS</button>
+					</div>
+				</div>
+			</div>
+			<div class="panel col-sm-3">
+				<div class="float-center">
+						<img class="bigPicture bp1" src="img/bayterek.jpg" width="100%"></img>
+				</div>
+				<div class="float-center">
+					<div class="panelText">
+						<h3 style="margin-top:15px;">BAITEREK<h3>
+						<p class="panelP">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+						<button type="button" class="btn btn-dark btn-lg">VIEW ALL TICKETS</button>
+					</div>
+				</div>
+			</div>
+			<div class="panel col-sm-3">
+				<div class="float-center">
+						<img class="bigPicture bp1" src="img/bayterek.jpg" width="100%"></img>
+				</div>
+				<div class="float-center">
+					<div class="panelText">
+						<h3 style="margin-top:15px;">BAITEREK<h3>
+						<p class="panelP">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+						<button type="button" class="btn btn-dark btn-lg">VIEW ALL TICKETS</button>
+					</div>
+				</div>
+			</div>
+			<div class="panel col-sm-3">
+				<div class="float-center">
+						<img class="bigPicture bp1" src="img/bayterek.jpg" width="100%"></img>
+				</div>
+				<div class="float-center">
+					<div class="panelText">
+						<h3 style="margin-top:15px;">BAITEREK<h3>
+						<p class="panelP">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+						<button type="button" class="btn btn-dark btn-lg">VIEW ALL TICKETS</button>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<div class="row">
+			<div class="panel col-sm-3">
+				<div class="float-center">
+						<img class="bigPicture bp1" src="img/bayterek.jpg" width="100%"></img>
+				</div>
+				<div class="float-center">
+					<div class="panelText">
+						<h3 style="margin-top:15px;">BAITEREK<h3>
+						<p class="panelP">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+						<button type="button" class="btn btn-dark btn-lg">VIEW ALL TICKETS</button>
+					</div>
+				</div>
+			</div>
+			<div class="panel col-sm-3">
+				<div class="float-center">
+						<img class="bigPicture bp1" src="img/bayterek.jpg" width="100%"></img>
+				</div>
+				<div class="float-center">
+					<div class="panelText">
+						<h3 style="margin-top:15px;">BAITEREK<h3>
+						<p class="panelP">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+						<button type="button" class="btn btn-dark btn-lg">VIEW ALL TICKETS</button>
+					</div>
+				</div>
+			</div>
+			<div class="panel col-sm-3">
+				<div class="float-center">
+						<img class="bigPicture bp1" src="img/bayterek.jpg" width="100%"></img>
+				</div>
+				<div class="float-center">
+					<div class="panelText">
+						<h3 style="margin-top:15px;">BAITEREK<h3>
+						<p class="panelP">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+						<button type="button" class="btn btn-dark btn-lg">VIEW ALL TICKETS</button>
+					</div>
+				</div>
+			</div>
+			<div class="panel col-sm-3">
+				<div class="float-center">
+						<img class="bigPicture bp1" src="img/bayterek.jpg" width="100%"></img>
+				</div>
+				<div class="float-center">
+					<div class="panelText">
+						<h3 style="margin-top:15px;">BAITEREK<h3>
+						<p class="panelP">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+						<button type="button" class="btn btn-dark btn-lg">VIEW ALL TICKETS</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	
+	
+		
+	</div>
+	
+	<div class="separator100px"></div>
+<footer>
+	<div class="container">
+		<div class="row">
+
+			<div class="col-sm-3">
+				<div class="float-right">
+					<h2>PLACES</h2>
+				</div>
+			</div>
+			
+			<div class="col-sm-6">
+				<div class="float-left" style="line-height: 2;">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.</p>
+				</div>
+				
+			</div>
+			
+			<div class="col-sm-3">
+				<div class="float-left">
+					<ul style="line-height: 2;">
+						<li>LINK</li>
+						<li>LINK</li>
+						<li>LINK</li>
+						<li>LINK</li>
+					</ul>
+				</div>
+				
+			</div>
+		</div>
+	</div>
+</footer>
+<script src="script.js"></script>
+</body>
+</html>
